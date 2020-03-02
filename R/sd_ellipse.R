22f91ce8458e6555a2d8055d5e0933e91bed2e2b
gavg712 <gavg712@gmail.com>
2020-03-02 18:47:54 -0500

    fixed control for weighting

--- R/sd_ellipse.R
+++ R/sd_ellipse.R
@@ -42,8 +42,6 @@ sd_ellipse <- function (points = NULL, x = NULL, y = NULL,
   }
   if (!is.null(points)) x <- points[[1]]
   if (!is.null(points)) y <- points[[2]]
-  weighted <- FALSE
-  if(!is.null(weights)) weighted <- TRUE
 
   n <- dim(points)[1]
   if (!is.null(centre)) {
@@ -62,7 +60,7 @@ sd_ellipse <- function (points = NULL, x = NULL, y = NULL,
   tmpAc2 <- tmpAc ^ 2
   tmpAcp <- purrr::map_dbl(seq_len(nrow(tmpA)), ~ prod(tmpAc[.x, ]))
 
-  if (weighted) {
+  if (!is.null(weights)) {
     top1 <- sum(rowSums(t(t(weights * tmpAc2) * c(1,-1))))
 
     top2 <- sqrt(sum(top1) ^ 2 + 4 * sum(weights * tmpAcp) ^ 2)
@@ -85,26 +83,20 @@ sd_ellipse <- function (points = NULL, x = NULL, y = NULL,
   sin2theta <- sintheta ^ 2
   cos2theta <- costheta ^ 2
   sinthetacostheta <- sintheta * costheta
-  if (weighted) {
+  if (!is.null(weights)) {
     sigmax <-
-      sqrt(2) * sqrt((sum(
-        colSums(weights * tmpAc2) * c(cos2theta, sin2theta)
-      ) -
+      sqrt(2) * sqrt((sum(colSums(weights * tmpAc2) * c(cos2theta, sin2theta)) -
         2 * sum(weights * tmpAcp) * sinthetacostheta) / sum(weights))
 
     sigmay <-
-      sqrt(2) * sqrt((sum(
-        colSums(weights * tmpAc2) * c(sin2theta, cos2theta)
-      ) +
+      sqrt(2) * sqrt((sum(colSums(weights * tmpAc2) * c(sin2theta, cos2theta)) +
         2 * (sum(weights * tmpAcp)) *  sinthetacostheta) / sum(weights))
   } else {
     sigmax <-
       sqrt(2) * sqrt((sum(colSums(tmpAc2) * c(cos2theta, sin2theta)) -
         2 * sum(tmpAcp) * sinthetacostheta) / (n - 2))
     sigmay <-
-      sqrt(2) * sqrt((sum(colSums(tmpAc2) * c(
-        sin2theta, cos2theta
-      )) +
+      sqrt(2) * sqrt((sum(colSums(tmpAc2) * c(sin2theta, cos2theta)) +
         2 * (sum(tmpAcp)) *  sinthetacostheta) /
         (n - 2))
   }
@@ -112,16 +104,14 @@ sd_ellipse <- function (points = NULL, x = NULL, y = NULL,
   if (sigmax > sigmay) {
     Major <- "SigmaX"
     Minor <- "SigmaY"
-  }
-  else {
+  }  else {
     Major <- "SigmaY"
     Minor <- "SigmaX"
   }
   lengthsigmax <- 2 * sigmax
   lengthsigmay <- 2 * sigmay
   areaSDE <- pi * sigmax * sigmay
-  eccentricity <- sqrt(1 - ((min(sigmax, sigmay) ^ 2) / (max(sigmax,
-                                                             sigmay) ^ 2)))
+  eccentricity <- sqrt(1 - ((min(sigmax, sigmay) ^ 2) / (max(sigmax, sigmay) ^ 2)))
   B <- min(sigmax, sigmay)
   A <- max(sigmax, sigmay)
   d2 <- (A - B) * (A + B)
@@ -133,20 +123,20 @@ sd_ellipse <- function (points = NULL, x = NULL, y = NULL,
   al <- (90 - theta) * pi / 180
   ca <- cos(al)
   sa <- sin(al)
-  coordsSDE <- t(t(xy %*% rbind(c(ca, sa), c(-sa, ca))) + centre)
+  coordsSDE <- xy %*% rbind(c(ca, sa), c(-sa, ca)) +
+    matrix(centre, nrow = 360, ncol = 2, byrow = TRUE)
   coordsSDE <- as.data.frame(coordsSDE)
   names(coordsSDE) <- names(points)[1:2]
   class(coordsSDE) <- c("sde_coords", class(coordsSDE))
 
   if (sigmax < sigmay) {
     Theta.Corr <- theta
-  }
-  else {
+  }  else {
     Theta.Corr <- theta + 90
   }
 
   result.sde <- as.data.frame(rbind(
-    c(weighted = weighted, centre = centre, Sigma.x = sigmax,
+    c(weighted = !is.null(weights), centre = centre, Sigma.x = sigmax,
       Sigma.y = sigmay, Theta = theta, Eccentricity = eccentricity,
       area.sde = areaSDE, tan_theta = tantheta, SinTheta = sintheta,
       CosTheta = costheta, SinThetaCosTheta = sinthetacostheta,
@@ -161,4 +151,5 @@ sd_ellipse <- function (points = NULL, x = NULL, y = NULL,
   if(length(out) == 1) out <- out[[1]]
   return(out)
 }
+
 
