682bc3cb7aa734f30989dd28337c325f021d065f
gavg712 <gavg712@gmail.com>
2024-03-11 12:16:50 -0500

    fix test errors

--- tests/testthat/test-mean.R
+++ tests/testthat/test-mean.R
@@ -2,15 +2,15 @@ test_that("mean center works", {
   x <- sf::st_sf(geometry = sf::st_sfc(list(sf::st_point(c(1, 1)),
                                             sf::st_point(c(2, 2)),
                                             sf::st_point(c(3, 3)))))
-  y <- st_coordinates(x)
-  expect_equal(st_coordinates(st_central_point(x)),
+  y <- sf::st_coordinates(x)
+  expect_equal(sf::st_coordinates(st_central_point(x)),
                matrix(apply(y, 2, mean), ncol = 2,
-                      dimnames = list(1, c("X", "Y"))))
-  expect_equal(st_coordinates(st_central_point(x, method = "median")),
+                      dimnames = list(NULL, c("X", "Y"))))
+  expect_equal(sf::st_coordinates(st_central_point(x, method = "median")),
                matrix(apply(y, 2, median), ncol = 2,
-                      dimnames = list(1, c("X", "Y"))))
-  expect_equal(st_coordinates(st_central_point(x, method = "feature")),
-               matrix(c(2, 2), ncol = 2, dimnames = list(1, c("X", "Y"))))
+                      dimnames = list(NULL, c("X", "Y"))))
+  expect_equal(sf::st_coordinates(st_central_point(x, method = "feature")),
+               matrix(c(2, 2), ncol = 2, dimnames = list(NULL, c("X", "Y"))))
   expect_equal(nrow(st_central_point(x)), 1)
   expect_error(st_central_point(y))
   expect_s3_class(st_central_point(x), class(x))
